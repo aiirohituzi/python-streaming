4f28cb02b42a7b0c13fd90be3baa3913f38d897b
aiirohituzi <sinn5656@gmail.com>
2017-06-16 23:24:23 +0900

    Add end event handler.

--- test2.py
+++ test2.py
@@ -7,13 +7,7 @@ class Download:
     @cherrypy.expose
     def index(self, file_):
         tgt = os.path.abspath(os.path.join('./music', file_))
-        return """
-<audio controls>
-  <source src="%s" type="audio/ogg">
-  <source src="%s" type="audio/mpeg">
-  Your browser does not support the audio tag.
-</audio>
-           """ % (serve_file(tgt, "application/x-download", "attachment"),)
+        return (serve_file(tgt, "application/x-download", "attachment"))
 
 if __name__ == '__main__':
     cherrypy.quickstart(Download())
--- test3.py
+++ test3.py
@@ -3,25 +3,32 @@ import pyglet
 
 
 def download_file(file_):
-    print(file_)
     data = {'file_': file_}
     r = requests.get("http://localhost:8080/index",
                      params=data,
                      stream=True
                      )
-
+    
+    print(r)
     local_filename = './tmp.mp3'
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
                 f.flush()
-    print(local_filename)
     return local_filename
 
+def some_function():
+    pyglet.app.exit()
+
 src = pyglet.media.StaticSource(pyglet.media.load(download_file('Intro.mp3')))
-print('aaaaaaaa')
 player = pyglet.media.Player()
 player.queue(src)
+player.push_handlers(on_eos=some_function)
 player.play()
 pyglet.app.run()
+
+print(player.playing)
+# pyglet.app.exit()
+print("333333333333")
+    
