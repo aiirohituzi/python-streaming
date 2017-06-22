24f5c6502f29194925adfc67b43ba81ddd0003ce
aiirohituzi <sinn5656@gmail.com>
2017-06-22 23:36:18 +0900

    Test to get mp3 stream.

--- audioStream/streaming/views.py
+++ audioStream/streaming/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import os
+from django.http import HttpResponse
 
 # Create your views here.
+def getAudio(request):
+    fname = os.path.abspath(os.path.join('../music', "Intro.mp3"))
+    f = open(fname, 'rb')
+
+    response = HttpResponse()
+    response.write(f.read())
+    response['Content-Type'] = 'audio/mp3'
+    response['Content-Length'] = os.path.getsize(fname)
+
+    return response
